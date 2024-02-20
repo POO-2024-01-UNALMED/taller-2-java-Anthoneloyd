13968bdf1f15724a2013fd5c6a2af9874341474d
Anthoneloyd <105695594+Anthoneloyd@users.noreply.github.com>
2024-02-19 21:32:42 -0500

    Añadir clases Asiento, Auto y Motor

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,32 @@
+//package test;
+
+public class Auto{
+    String modelo;
+    int precio;
+    Asiernto [] asientos;
+    String marca;
+    Motor motor;
+    int registro;
+    static int cantidadCreados;
+
+    public int cantidadAsientos() {
+        int cantidad = 0;
+        for (int i = 0; i < this.asientos.length; i++) {
+            if (this.asientos[i] != null) {
+                cantidadCreados++;
+            }
+        }
+    }
+
+    public String verificarIntegridad(){
+        for(int i = 0; i < this.asientos.length; i++){
+            if(this.asientos[i] != null) {
+                if (this.registro != this.asientos[i].registro || this.motor.registro != this.asientos[i].registro) {
+                    return "Las piezas no son originales";
+                }
+            }
+        }
+
+        return "Auto original";
+    }
+}
